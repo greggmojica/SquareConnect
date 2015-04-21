5592a3730822e3f9deddfa2d22464cedec41fcbf
Gregg Mojica <gwmojica@gmail.com>
2015-04-21 19:30:27 -0400

    Inital Commit

--- Square-Connect/ViewController.m
+++ Square-Connect/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking/AFNetworking.h"
 
 @interface ViewController ()
 
@@ -16,6 +17,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager setRequestSerializer:[AFJSONRequestSerializer serializer]];
+    
+    [manager.requestSerializer setValue:@"Bearer XXXXX" forHTTPHeaderField:@"Authorization"];
+    [manager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+    NSDictionary *parameters = @{@"quantity_delta": [NSNumber numberWithInt:-1], @"adjustment_type": @"SALE"};
+    NSLog(@"Parameters are %@", parameters);
+    
+    [manager POST:@"https://connect.squareup.com/v1/me/items/00CC0141-C50C-4A7A-9FAA-368EC79DA652" parameters:@{@"quantity_delta": [NSNumber numberWithInt:-1], @"adjustment_type": @"SALE"} success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"JSON: %@", responseObject);
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
