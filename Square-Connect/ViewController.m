701db56bd63675f815ba1b134afb9deb83e6b9e4
Gregg Mojica <gwmojica@gmail.com>
2015-04-21 21:41:43 -0400

    Added /inventory hyperlink

--- Square-Connect/ViewController.m
+++ Square-Connect/ViewController.m
@@ -22,13 +22,13 @@
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     [manager setRequestSerializer:[AFJSONRequestSerializer serializer]];
     
-    [manager.requestSerializer setValue:@"Bearer XXXXX" forHTTPHeaderField:@"Authorization"];
+    [manager.requestSerializer setValue:@"Bearer " forHTTPHeaderField:@"Authorization"];
     [manager.requestSerializer setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
     
     NSDictionary *parameters = @{@"quantity_delta": [NSNumber numberWithInt:-1], @"adjustment_type": @"SALE"};
     NSLog(@"Parameters are %@", parameters);
     
-    [manager POST:@"https://connect.squareup.com/v1/me/items/00CC0141-C50C-4A7A-9FAA-368EC79DA652" parameters:@{@"quantity_delta": [NSNumber numberWithInt:-1], @"adjustment_type": @"SALE"} success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    [manager POST:@"https://connect.squareup.com/v1/me/inventory/00CC0141-C50C-4A7A-9FAA-368EC79DA652" parameters:@{@"quantity_delta": [NSNumber numberWithInt:-1], @"adjustment_type": @"SALE"} success:^(AFHTTPRequestOperation *operation, id responseObject) {
         NSLog(@"JSON: %@", responseObject);
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
         NSLog(@"Error: %@", error);
